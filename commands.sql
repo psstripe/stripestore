f9d5794459498c1c84bc54ac56ec898030904aae
Pamela Shen <pamela@stripe.com>
2016-10-06 19:53:45 -0700

    sql commands

---
+++ commands.sql
@@ -0,0 +1,9 @@
+/* This is a comment. */
+CREATE TABLE product (
+  id INTEGER PRIMARY KEY,
+  product_name VARCHAR(255),
+  price INTEGER
+);
+
+INSERT INTO product VALUES (1, "sneakers", 10000), (2, "sandals", 15000), (3, "heels",30000);
+INSERT INTO product VALUES (4, "flip flops", 20000), (5, "high tops", 15000), (6, "wedges",30000);
